ab0fab97ca961daa8cfb4f87880670a115a974fd
dongbingwei <dongbingwei@lsh123.com>
2016-12-08 22:06:13 +0800

    增加token

--- ACTIVITY-H5/activity/operation/shake/api.py
+++ ACTIVITY-H5/activity/operation/shake/api.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import request, current_app, json
+from flask import request, current_app, json, make_response
 
 from ..base_operation import activity
 from .. import BaseAction, RETStatus
@@ -30,14 +30,16 @@ def get_shake_static_data():
     shake_service = ShakeService()
     if type(_get_param()) == tuple:
         return _get_param()
-    activity_id, uid = _get_param()
+    activity_id, uid, token = _get_param()
     # 获取静态数据
     static_resp = shake_service.get_static_data(activity_id, uid)
     static_resp = json.loads(static_resp)
     if 'none' in static_resp and static_resp['none'] is True:
         if static_resp['type'] == 1:
             return BaseAction.jsonify_with_data('', RETStatus.NO_ACTIVITY)
-    return BaseAction.jsonify_with_data(static_resp)
+    resp = make_response(BaseAction.jsonify_with_data(static_resp))
+    resp.set_cookie('token', token)
+    return resp
 
 
 @activity.route('/shake/play', methods=['GET', 'POST'])
@@ -45,27 +47,39 @@ def shake_play():
     shake_service = ShakeService()
     if type(_get_param()) == tuple:
         return _get_param()
-    activity_id, uid = _get_param()
+    activity_id, uid, token = _get_param()
     # 获取抽奖结果
     play_resp = shake_service.shake_play(activity_id, uid)
     return BaseAction.jsonify_with_data(play_resp)
 
-@activity.route('/shake/count', methods=['GET', 'POST'])
-def shake_count():
-    pass
 
 def _get_param():
-    if request.method == 'POST':
-        # 验证请求参数的个数
-        if len(request.form) != 2:
-            return BaseAction.jsonify_with_data('', RETStatus.PARMA_ERROR)
-        activity_id = request.form['activity_id']
-        token = request.form['token']
+    # 在cookie中获取token
+    token = request.cookies.get('token')
+    # 判断cookie是否含有token
+    if token is None:
+        if request.method == 'POST':
+            # 验证请求参数的个数
+            if len(request.form) != 2:
+                return BaseAction.jsonify_with_data('', RETStatus.PARMA_ERROR)
+            activity_id = request.form['activity_id']
+            token = request.form['token']
+        else:
+            if len(request.args) != 2:
+                return BaseAction.jsonify_with_data('', RETStatus.PARMA_ERROR)
+            activity_id = request.args.get('activity_id')
+            token = request.args.get('token')
+
+    # cookie中含有token只获取activity_id
     else:
-        if len(request.args) != 2:
-            return BaseAction.jsonify_with_data('', RETStatus.PARMA_ERROR)
-        activity_id = request.args.get('activity_id')
-        token = request.args.get('token')
+        if request.method == "POST":
+            if len(request.form) != 1:
+                return BaseAction.jsonify_with_data('', RETStatus.PARMA_ERROR)
+            activity_id = request.form['activity_id']
+        else:
+            if len(request.args) != 1:
+                return BaseAction.jsonify_with_data('', RETStatus.PARMA_ERROR)
+            activity_id = request.args.get('activity_id')
     # 验证token
     user_info = BaseAction.check_token(token)
     if user_info is None:
@@ -75,4 +89,4 @@ def _get_param():
     # 验证参数中的值是否为空
     if len(activity_id) == 0 or len(token) == 0:
         return BaseAction.jsonify_with_data('', RETStatus.PARMA_ERROR)
-    return [activity_id, uid]
+    return [activity_id, uid, token]
