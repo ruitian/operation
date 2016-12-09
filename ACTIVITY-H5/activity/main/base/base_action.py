8963326ed7bde5da7bb9940fcd4523ff3396ea3c
dongbingwei <dongbingwei@lsh123.com>
2016-12-09 16:39:31 +0800

    token请求日志

--- ACTIVITY-H5/activity/main/base/base_action.py
+++ ACTIVITY-H5/activity/main/base/base_action.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import urllib2
 import urllib
+import time
 from flask import current_app, jsonify, json
 
 from ..common import APIStatus, RETStatus
@@ -10,15 +11,25 @@ class BaseAction(object):
 
     @staticmethod
     def check_token(token=None):
+        start_time = time.time()
+        urllib2_log = current_app.config['LOG_FILE'] + 'urllib2.log'
+
         data = urllib.urlencode({
             'token': token
         })
         try:
             url = current_app.config['SERVICE_URL'] + '/account/user/tokenlogin'
             resp = json.loads(urllib2.urlopen(url, data=data, timeout=15).read())
+            # 请求时间
+
         except Exception, e:
-            print str(e)
+            process_time = time.time() - start_time
+            with open(urllib2_log, 'a') as f:
+                f.write(str(time.ctime()) + ' ' + url + ' ' + str(process_time) +' ' + str(e) +'\n')
         else:
+            process_time = time.time() - start_time
+            with open(urllib2_log, 'a') as f:
+                f.write(str(time.ctime()) + ' ' + url + ' ' + str(process_time) + '\n')
             if resp['ret'] != 0:
                 return None
             return resp['content']
